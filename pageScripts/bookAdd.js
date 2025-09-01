2f335532caacd2f68f68966edfe787c7f5aba2a3
wiljoyaa <wiljoyaa@gmail.com>
2025-09-01 02:15:40 -0400

    add book button
    added button to add books but currently only functions as a hello world button

---
+++ pageScripts/bookAdd.js
@@ -0,0 +1,6 @@
+const addBook = document.querySelector('#addBook')
+const librarySpace = document.querySelector('#bookSpace')
+
+addBook.addEventListener('click', function(e) {
+    console.log('Hello World!');
+})
