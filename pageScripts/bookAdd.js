dc5c6ad69568538a8818c06bf076e94b178ff014
wiljoyaa <wiljoyaa@gmail.com>
2025-09-10 14:49:29 -0400

    Add and Remove Button
    added functionality to the add button for removing books from the library and created the remove button and started adding its logic

--- pageScripts/bookAdd.js
+++ pageScripts/bookAdd.js
@@ -1,6 +1,35 @@
 const addBook = document.querySelector('#addBook')
+const removeBook = document.querySelector('#removeBook')
 const librarySpace = document.querySelector('#bookSpace')
 
+
+/* Add book method */
 addBook.addEventListener('click', function(e) {
-    console.log('Hello World!');
+    /* Retrieve book name */
+    let bookName = prompt("Please enter a book name");
+
+    /* Create new book element */
+    const newBookCard = document.createElement("div");
+    newBookCard.classList.add("book-card");
+    const newBook = document.createElement("div");
+    newBook.classList.add("book-title");
+    newBook.textContent = bookName;
+
+    /* Add new book to bookshelf */
+    newBookCard.appendChild(newBook);
+    librarySpace.appendChild(newBookCard);
+})
+
+
+/* Remove Book method */
+removeBook.addEventListener('click', function(e){
+    /* Retrieve book name */
+    let bookName = prompt("Please enter the book you want to remove");
+
+    /* get bookshelf and all book cards */
+    const bookShelf = document.getElementById("#bookSpace");
+    const cards = bookShelf.children;
+
+
+    
 })
